244a3fe3b2c160857374c9ba76ddd377df584eee
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2023-03-18 16:57:13 +0400

    new section

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './assets/style/index.scss'
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <h1>Hello word</h1>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, iste?</h2>
+      <h3>hello Davit </h3>
+    </div>
+  }
+}
+
+export default App
